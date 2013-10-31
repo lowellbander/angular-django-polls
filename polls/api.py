defff381998a2413199e55cf68c20dc3ac1b3482
lowellbander <Lowell@new-host.home>
2013-10-31 00:16:50 -0700

    added auth

--- polls/api.py
+++ polls/api.py
@@ -1,10 +1,13 @@
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from polls.models import Poll, Choice
 
 class PollResource(ModelResource):
 	class Meta:
 		queryset = Poll.objects.all()
+		authorization= Authorization()
 
 class ChoiceResource(ModelResource):
 	class Meta:
 		queryset = Choice.objects.all()
+		authorization= Authorization()
